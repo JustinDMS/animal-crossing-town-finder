e004ca99e62f89ad62b40867ce3b0e5624d6a6e5
JustinDMS <90155396+JustinDMS@users.noreply.github.com>
2026-05-22 15:11:37 -0500

    Implement town data output to binary
    - Writes in batches of 10k
    - This only improved performance by 3 seconds (151->148) per 1,000,000 towns

--- src/main.c
+++ src/main.c
@@ -1,35 +1,52 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include "../include/rand.h"
 #include "../include/generator.h"
 
-int main(int argc, char* argv[]) {
-	uint32_t iter = UINT32_MAX;
+#define BATCH_SIZE 10000
 
-	/*FILE* file = NULL;
-	file = fopen("results.txt", "a");
-	if (file == NULL) {
-		return 1;
-	}*/
+int main(int argc, char* argv[]) {
+	struct TownData* batch = malloc(BATCH_SIZE * sizeof(struct TownData));
+	if (batch == NULL) {
+		printf("Failed to allocate enough space");
+		return 2;
+	}
+
+	FILE* db = fopen("towns.bin", "wb");
+	if (db == NULL) {
+		printf("Failed to open database file");
+		free(batch);
+		return 2;
+	}
+
+	int batch_count = 0;
 
 	clock_t start, end;
 	start = clock();
 
-	for (uint32_t i = 0; i < iter; i++) {
-		set_random_seed(i);
-		//printf("\nSeed %X", get_random_seed());
-		uint32_t cur_seed = i;
-
-		int result = generate();
-		//printf("%X | Found valid town!\n", cur_seed);
+	for (uint32_t seed = 0; seed <= 0; seed++) {
+		set_random_seed(seed);
+		batch[batch_count].seed = seed;
+		(void)generate(&batch[batch_count]);
+		batch_count++;
+		
+		if (batch_count == BATCH_SIZE) {
+			fwrite(batch, sizeof(struct TownData), BATCH_SIZE, db);
+			batch_count = 0;
+		}
 	}
-
-	/*fprintf(file, "----------\n");
-	fclose(file);*/
+	if (batch_count > 0) {
+		fwrite(batch, sizeof(struct TownData), batch_count, db);
+	}
 
 	end = clock();
 	double time_used = ((double)(end - start) / CLOCKS_PER_SEC);
-	printf("\nFinished generating %d towns in %f seconds", iter, time_used);
+
+	fclose(db);
+	free(batch);
+
+	printf("\nFinished generating in %f seconds", time_used);
 	
 	return 0;
 }
